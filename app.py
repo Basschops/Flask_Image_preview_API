d08aed8402cd5550a015eb921539b1a52c8638cd
Daragh <daraghofarrell@gmail.com>
2019-03-13 20:15:46 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+import urllib.request as request
+import urllib.error
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_jsonpify import jsonify
+import os
+import platform
+
+
+app = Flask(__name__)
+api = Api(app)
+
+# API that pings pond5 website. Returns "Pong" if successful
+class Ping(Resource):
+    def get(self):
+        URL = "www.pond5.com"
+        ping = os.system(" ping -c 1 " + URL)
+        # Success
+        if ping==0:
+            return "Pong"
+        # Failure
+        else:
+            return "Unable to ping"
+
+# API that returns information about the system running in JSON format
+class System(Resource):
+    def get(self):
+        data = {'system': platform.uname().system,
+                'release': platform.release(),
+                'version': platform.uname().version,
+                'architecture': platform.architecture(),
+                'machine': platform.machine()}
+        return jsonify(data)
+
+# API that returns preview information about a media picture from pond5.com
+class Media_info(Resource):
+    # media_id is the numbered code of the specific photo resource
+    def get(self, media_id):
+        URL = "https://www.pond5.com/photo/" + str(media_id)
+
+        try:
+            response = request.urlopen(URL)
+        except urllib.error.URLError as e:
+            return e.reason
+
+        soup = BeautifulSoup(response, 'html.parser')
+        icon = soup.find('img')
+        # Return alt as title and source of image
+        data = {'name': icon['alt'], 'source': icon['src']}
+
+        return jsonify(data)
+
+# Define routes
+api.add_resource(Ping, '/ping')
+api.add_resource(System, '/system')
+api.add_resource(Media_info, '/mediainfo/<media_id>')
+
+# Basic homepage information
+@app.route("/")
+def home():
+    html = "<h3>Welcome!</h3><br><p>Navigate to /ping to ping Pond5 site.</p><br>\
+    <p>Navigate to /system to display system information.</p><br>\
+    <p>Navigate to /mediainfo/media_id to get preview information of an image\
+    where media_id is the resource id number.</p>"
+    return html
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ test_app.py
@@ -0,0 +1,89 @@
+import unittest
+import os
+import json
+import sys
+import app
+import platform
+
+
+class TestApp(unittest.TestCase):
+
+    # Set up app test client to test api calls
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_ping(self):
+        # Google is considered a trusted site that should work almost always
+        # This will test if google is accessible, so should pond5.com
+        # and if not, this is a crude test that there is not internet access.
+        URL = "www.google.com"
+        ping = os.system(" ping -c 1 " + URL)
+        if ping==0:
+            testPing =  '"Pong"'
+        else:
+            testPing = '"Unable to ping"'
+        response = self.app.get('/ping')
+        self.assertEqual(response.get_data().decode().strip(),testPing)
+
+    def test_media_response(self):
+        # Test the that two samples retrun the correct information
+        responseA = self.app.get('/mediainfo/11497188')
+        responseB = self.app.get('/mediainfo/11497190')
+
+        self.assertEqual(
+        json.loads(responseA.get_data().decode(sys.getdefaultencoding()))['name'],
+         "Samuel Beckett Bridge in Dublin, Ireland. Stock Photos")
+
+        self.assertEqual(
+        json.loads(responseA.get_data().decode(sys.getdefaultencoding()))['source'],
+         "https://images.pond5.com/samuel-beckett-bridge-dublin-ireland-photo-011497188_iconl.jpeg")
+
+        self.assertEqual(
+         json.loads(responseB.get_data().decode(sys.getdefaultencoding()))['name'],
+          "Protest in Dublin, Ireland. Stock Photos")
+
+        self.assertEqual(
+         json.loads(responseB.get_data()
+         .decode(sys.getdefaultencoding()))['source'],
+          "https://images.pond5.com/protest-dublin-ireland-photo-011497190_iconl.jpeg")
+
+    def test_system_response(self):
+        # Test that the system information is being transmitted correctly
+        # through the API
+        system = platform.uname().system
+        release = platform.release()
+        version = platform.uname().version
+        architecture = platform.architecture()
+        processor = platform.machine()
+
+        response = self.app.get('/system')
+
+        self.assertEqual(
+         json.loads(response.get_data()
+         .decode(sys.getdefaultencoding()))['system'], system)
+
+        self.assertEqual(
+         json.loads(response.get_data()
+         .decode(sys.getdefaultencoding()))['release'], release)
+
+        self.assertEqual(
+         json.loads(response.get_data()
+         .decode(sys.getdefaultencoding()))['version'], version)
+
+        # Architecture returns tuple or list so compare each value to avoid
+        # type errors.
+        self.assertEqual(
+         json.loads(response.get_data()
+         .decode(sys.getdefaultencoding()))['architecture'][0], architecture[0])
+
+        self.assertEqual(
+         json.loads(response.get_data()
+         .decode(sys.getdefaultencoding()))['architecture'][1], architecture[1])
+
+        self.assertEqual(
+         json.loads(response.get_data()
+         .decode(sys.getdefaultencoding()))['processor'], processor)
+
+
+if __name__ == '__main__':
+    unittest.main()
